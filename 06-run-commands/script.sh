aa619591f90964746536b22cf77f120f8a708eb0
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-07-28 17:31:06 +0600

    run commands

--- 06-run-commands/script.sh
+++ 06-run-commands/script.sh
@@ -3,3 +3,13 @@ whoami
 ls
 pwd
 
+
+if [ -f /etc/os-release ]; then
+  source /etc/os-release
+  echo "Distribution: $ID"
+elif [ -f /etc/lsb-release ]; then
+  source /etc/lsb-release
+  echo "Distribution: $DISTRIB_ID"
+else
+  echo "Distribution: Unknown"
+fi
